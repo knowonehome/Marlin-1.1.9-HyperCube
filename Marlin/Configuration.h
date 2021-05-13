64150b7b8c69020a5390d4a580058070a33885a1
Kenny Peel <knowonehome@gmail.com>
2021-05-13 11:23:31 -0500

    updated max feedrate
    https://youtu.be/tK_YqSbDpZ4?t=517

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -619,7 +619,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 5, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 500, 500, 30, 100 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
